8ba3abece34acf4e39a448de5fcda491777edd6e
Pupett <pupettaj@gmail.com>
2024-03-07 02:35:22 -0500

    Migrated old canvas code to legacy.js, and added + modified a new spiral canvas background in main.js

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -1,124 +1,179 @@
-const canvas = document.getElementById("canvas"),
-  ctx = canvas.getContext("2d");
+// CREDIT FOR ORIGINAL CODE:
+// https://codepen.io/hakimel/pen/QdWpRv?utm_source=extension&utm_medium=click&utm_campaign=muzli
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d");
 
-// console.log(canvas.height);
+let time = 0,
+  velocity = 0.1,
+  velocityTarget = 0.01,
+  width,
+  height,
+  lastX,
+  lastY,
+  r1;
 
-const winWidth = window.innerWidth;
-let dotCount;
+// Spacing options
+// 4, 5, 6, 8,
 
-winWidth > 1600
-  ? (dotCount = 350)
-  : winWidth > 1300
-  ? (dotCount = 300)
-  : winWidth > 1100
-  ? (dotCount = 200)
-  : winWidth > 800
-  ? (dotCount = 100)
-  : winWidth > 600
-  ? (dotCount = 80)
-  : (dotCount = 100);
+// 0.9
+const MAX_OFFSET = 450;
+const SPACING = 4;
+const POINTS = Math.floor(MAX_OFFSET / SPACING);
+const PEAK = MAX_OFFSET;
+const POINTS_PER_LAP = 5;
+const SHADOW_STRENGTH = 10;
 
-const stars = [], // Array that contains the stars
-  FPS = 60, // Frames per second
-  dots = dotCount, // Number of stars
-  mouse = {
-    x: 0,
-    y: 0,
-  }; // mouse location
+setup();
 
-// Push stars to array
+function setup() {
+  resize();
+  step();
 
-for (let i = 0; i < dots; i++) {
-  stars.push({
-    x: Math.random() * canvas.width,
-    y: Math.random() * canvas.height,
-    radius: Math.random() * 1 + 1,
-    vx: Math.floor(Math.random() * 50) - 25,
-    vy: Math.floor(Math.random() * 50) - 25,
-  });
+  window.addEventListener("resize", resize);
+  window.addEventListener("mousedown", onMouseDown);
+  document.addEventListener("touchstart", onTouchStart);
 }
 
-// Draw the scene
-// o(n^2 + n) can this be done better?
-function draw() {
-  const grd = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
-  grd.addColorStop(0, "rgb(80, 102, 228");
-  grd.addColorStop(1, "rgb(248, 108, 253");
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+function resize() {
+  width = canvas.width = window.innerWidth;
+  height = canvas.height = window.innerHeight;
 
-  ctx.globalCompositeOperation = "lighter";
-
-  for (let i = 0, x = stars.length; i < x; i++) {
-    const s = stars[i];
-
-    ctx.fillStyle = grd;
-    ctx.beginPath();
-    ctx.arc(s.x, s.y, s.radius, 0, 2 * Math.PI);
-    ctx.fill();
-    ctx.fillStyle = "black";
-    ctx.stroke();
+  if (window.innerWidth > 1500) {
+    r1 = window.innerWidth * 0.5;
+  } else if (window.innerWidth <= 1500 && window.innerWidth > 1000) {
+    r1 = 700;
+  } else if (window.innerWidth <= 1000) {
+    r1 = 450;
   }
+}
 
-  ctx.beginPath();
-  for (let i = 0, x = stars.length; i < x; i++) {
-    const starI = stars[i];
-    ctx.moveTo(starI.x, starI.y);
+function step() {
+  time += velocity;
+  velocity += (velocityTarget - velocity) * 0.3;
 
-    if (distance(mouse, starI) < 150) ctx.lineTo(mouse.x, mouse.y);
+  clear();
+  render();
 
-    for (let j = 0, x = stars.length; j < x; j++) {
-      const starII = stars[j];
-      if (distance(starI, starII) < 150) {
-        ctx.lineTo(starII.x, starII.y);
-      }
+  requestAnimationFrame(step);
+}
+
+function clear() {
+  context.clearRect(0, 0, width, height);
+}
+
+function render() {
+  let x,
+    y,
+    cx = width / 2,
+    cy = height / 2;
+
+  // Gradient
+  grd = context.createRadialGradient(cx, cy, r1, width, height, 30);
+
+  grd.addColorStop(0.5, "rgb(248, 108, 253");
+  grd.addColorStop(0.6, "rgb(80, 102, 228");
+  grd.addColorStop(0.2, "rgb(80, 102, 228");
+
+  context.globalCompositeOperation = "lighter";
+  context.strokeStyle = grd;
+  context.shadowColor = grd;
+  context.lineWidth = 2;
+  context.beginPath();
+
+  // 112
+
+  for (let i = POINTS; i > 0; i--) {
+    let value = i * SPACING + (time % SPACING);
+
+    const ax = Math.sin(value / POINTS_PER_LAP) * Math.PI,
+      ay = Math.cos(value / POINTS_PER_LAP) * Math.PI * 0.9;
+
+    (x = ax * value), (y = ay * value * 0.35);
+
+    const o = 1 - Math.floor(PEAK / PEAK);
+
+    y -= Math.pow(o, 2) * 200;
+    y += (200 * value) / MAX_OFFSET;
+    y += (x / cx) * width * 0.1;
+
+    y -= 15;
+
+    y = Math.floor(y);
+    if (i === 112) {
+      y = cy;
     }
+
+    // const center = Math.floor(POINTS / 2);
+    // if (i === center) {
+    //   y = cy;
+    // }
+
+    context.globalAlpha = 1 - value / MAX_OFFSET;
+    context.shadowBlur = SHADOW_STRENGTH * o;
+
+    context.lineTo(cx + x, cy + y);
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(cx + x, cy + y);
   }
-  ctx.lineWidth = 0.05;
-  ctx.strokeStyle = grd;
-  ctx.stroke();
+
+  // context.lineTo(cx, cy - 500);
+  // context.lineTo(cx, 0);
+  // context.stroke();
 }
 
-function distance(point1, point2) {
-  let xs = 0;
-  let ys = 0;
+function onMouseDown(event) {
+  lastX = event.clientX;
+  lastY = event.clientY;
 
-  xs = point2.x - point1.x;
-  xs = xs * xs;
-
-  ys = point2.y - point1.y;
-  ys = ys * ys;
-
-  return Math.sqrt(xs + ys);
+  document.addEventListener("mousemove", onMouseMove);
+  document.addEventListener("mouseup", onMouseUp);
 }
 
-// Update star locations
-function update() {
-  for (let i = 0, x = stars.length; i < x; i++) {
-    const s = stars[i];
+function onMouseMove(event) {
+  let vx = (event.clientX - lastX) / 100;
+  let vy = (event.clientY - lastY) / 100;
 
-    s.x += s.vx / FPS;
-    s.y += s.vy / FPS;
+  if (event.clientY < height / 2) vx *= -1;
+  if (event.clientX > width / 2) vy *= -1;
 
-    if (s.x < 0 || s.x > canvas.width) s.vx = -s.vx;
-    if (s.y < 0 || s.y > canvas.height) s.vy = -s.vy;
-  }
+  velocityTarget = vx + vy;
+
+  lastX = event.clientX;
+  lastY = event.clientY;
 }
 
-const rect = canvas.getBoundingClientRect();
-window.addEventListener("mousemove", (e) => {
-  mouse.x = e.clientX - rect.left;
-  mouse.y = e.clientY - rect.top;
-});
-
-// Update and draw
-
-function tick() {
-  draw();
-  update();
-  requestAnimationFrame(tick);
+function onMouseUp() {
+  document.removeEventListener("mousemove", onMouseMove);
+  document.removeEventListener("mouseup", onMouseUp);
 }
 
-tick();
+function onTouchStart(event) {
+  event.preventDefault();
+
+  lastX = event.touches[0].clientX;
+  lastY = event.touches[0].clientY;
+
+  document.addEventListener("touchmove", onTouchMove);
+  document.addEventListener("touchend", onTouchEnd);
+}
+
+function onTouchMove(event) {
+  let vx = (event.touches[0].clientX - lastX) / 100;
+  let vy = (event.touches[0].clientY - lastY) / 100;
+
+  if (event.touches[0].clientY < height / 2) vx *= -1;
+  if (event.touches[0].clientX > width / 2) vy *= -1;
+
+  velocityTarget = vx + vy;
+
+  lastX = event.touches[0].clientX;
+  lastY = event.touches[0].clientY;
+}
+
+function onTouchEnd() {
+  document.removeEventListener("touchmove", onTouchMove);
+  document.removeEventListener("touchend", onTouchEnd);
+}
